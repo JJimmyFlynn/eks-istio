d64fa4f67c63f20287281989ab391e77c2a39039
John Flynn <jjimmyflynn@gmail.com>
2025-02-25 18:54:53 -0500

    Add vars to init steps

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,3 +4,4 @@ set -eou pipefail
 aws eks update-kubeconfig --name fly-eks
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -f ./k8s/argo-apps/

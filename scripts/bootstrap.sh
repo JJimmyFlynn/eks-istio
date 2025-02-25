ee9d1e4473ac884b3cb8db8f49fc47601dd967fb
John Flynn <jjimmyflynn@gmail.com>
2025-02-25 18:15:14 -0500

    Initial commit

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+set -eou pipefail
+
+aws eks update-kubeconfig --name fly-eks
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
